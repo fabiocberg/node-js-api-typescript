371730a656125334e1d75cfe97522f5f6679404a
Fabio Bergmann <fabiocberg@gmail.com>
2021-08-14 08:50:51 -0300

    login

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,40 @@
 import express from 'express'
+import * as jwt from 
+    'jsonwebtoken'
 
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send('OK')
+const getToken = (user) => {
+    const payload = {
+        id: user.id
+    }
+    return jwt.sign(
+        payload,
+        'secret',
+        {
+            expiresIn: '1h'
+        }
+    )
+}
+
+app.post('/login', 
+    (req, res) => {
+    // Passar os dados para
+    // um controller, que
+    // verifica os dados do
+    // usuário e retorna
+    // o token.
+    const user = {
+        id: 1,
+        name: 'João',
+        email: 'joao@gmail.com'
+    }
+    const token = getToken(user)
+    return res.json({token})
 })
 
 app.listen(3333, () => {
-    console.log('Server is running.')
+    console.log(
+        'Server is running.'
+    )
 })
